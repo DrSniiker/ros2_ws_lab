2659c43433c66fb5229b4a75a15cfd6322a4308f
Albin Bachler Jansson <albin.bachler.jansson@gmail.com>
2025-04-25 10:37:40 +0200

    Ja

---
+++ lab2/build/bringup/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/rosdev/ros2_ws_lab/lab2/src/bringup
+# Build directory: /home/rosdev/ros2_ws_lab/lab2/build/bringup
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
