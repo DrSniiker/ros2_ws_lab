e6703198ecbd6148c9934c20a0779382d5460f98
Albin Bachler Jansson <albin.bachler.jansson@gmail.com>
2025-05-23 10:27:41 +0200

    asdf

---
+++ lab3/build/dynamixel_sdk/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/rosdev/ros2_ws_lab/lab3/src/DynamixelSDK/ros/dynamixel_sdk
+# Build directory: /home/rosdev/ros2_ws_lab/lab3/build/dynamixel_sdk
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
