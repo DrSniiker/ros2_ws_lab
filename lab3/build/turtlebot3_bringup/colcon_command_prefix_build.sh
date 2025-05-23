12b463a714bf667974c0b10bd06a0f25c19833d8
Albin Bachler Jansson <albin.bachler.jansson@gmail.com>
2025-05-23 10:26:09 +0200

    fixed_

--- lab3/build/turtlebot3_bringup/colcon_command_prefix_build.sh
+++
@@ -1,5 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/rosdev/ros2_ws_lab/lab3/install/dynamixel_sdk/share/dynamixel_sdk/package.sh"
-. "/home/rosdev/ros2_ws_lab/lab3/install/turtlebot3_description/share/turtlebot3_description/package.sh"
-. "/home/rosdev/ros2_ws_lab/lab3/install/turtlebot3_msgs/share/turtlebot3_msgs/package.sh"
-. "/home/rosdev/ros2_ws_lab/lab3/install/turtlebot3_node/share/turtlebot3_node/package.sh"
--- lab3/build/turtlebot3_navigation2/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rosdev/ros2_ws_lab/lab3/src/turtlebot3/turtlebot3_navigation2
-# Build directory: /home/rosdev/ros2_ws_lab/lab3/build/turtlebot3_navigation2
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
